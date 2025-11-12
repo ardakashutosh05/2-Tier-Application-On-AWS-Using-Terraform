b6614355cb05d4c7234e928b19b5edef81997cf6
Ashutosh Ardak <ardakashutosh05@gmail.com>
2025-11-12 16:08:20 +0000

    update

--- root/backend.tf
+++ root/backend.tf
@@ -2,7 +2,8 @@ terraform {
   backend "s3" {
     bucket = "2-tier-terraform-state-bucket"
     key    = "backend/10weeksofcloudops-demo.tfstate"
-    region = "us-east-1"
-    dynamodb_table = "remote-backend"
+    region = "ap-southeast-1"
+    dynamodb_table = "ashu-table-lock"
+
   }
 }
